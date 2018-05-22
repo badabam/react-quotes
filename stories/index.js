896bb48c70c70b9989fa8c8a09f4e560e4011955
Jeremias Erbs <jerry@badabam.de>
2018-05-22 21:40:27 +0200

    Add knobs addon for Storybook

--- stories/index.js
+++ stories/index.js
@@ -1,22 +1,35 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs'
+
 import Quote from '../src/components/Quote'
+import Notification from '../src/components/Notification'
 
 storiesOf('Quote', module)
+  .addDecorator(withKnobs)
   .add('with short text', () => (
     <Quote
-      text="Hello world"
-      author="Jerry"
+      text={text('Copy', 'Hello world')}
+      author={text('Author name', 'Jerry')}
       onLike={action('liked')}
-      numLikes={3}
+      numLikes={number('Number of likes', 3)}
     />
   ))
   .add('with long text', () => (
     <Quote
-      text="Lorem ipsum dolor sit. This is a longer quote."
-      author="Jerry"
+      text={text('Copy', 'Lorem ipsum dolor sit. This is a longer quote.')}
+      author={text('Author name', 'Jerry')}
       onLike={action('liked')}
-      numLikes={3}
+      numLikes={number('Number of likes', 0)}
+    />
+  ))
+
+storiesOf('Notification', module)
+  .addDecorator(withKnobs)
+  .add('default', () => (
+    <Notification
+      show={boolean('Show Notification', true)}
+      onClose={action('closed')}
     />
   ))
