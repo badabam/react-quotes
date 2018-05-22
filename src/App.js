931b073f57ea29e81cdccbf9de855d20ea6e0d66
Jeremias Erbs <jerry@badabam.de>
2018-05-22 12:15:40 +0200

    Move like-state up to App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,40 @@ import React, { Component } from 'react'
 import Quote from './components/Quote'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      firstQuoteLikes: 0,
+      secondQuoteLikes: 0,
+    }
+  }
+
+  increaseFirstLikes() {
+    this.setState({ firstQuoteLikes: this.state.firstQuoteLikes + 1 })
+  }
+
+  increaseSecondLikes() {
+    this.setState({ secondQuoteLikes: this.state.secondQuoteLikes + 1 })
+  }
+
   render() {
+    const totalLikes = this.state.firstQuoteLikes + this.state.secondQuoteLikes
     return (
       <div>
-        <h1>Some quotes:</h1>
-        <Quote text="It is how it is" author="Jerry" />
-        <Quote text="props are immutable" author="Some React-Guy" />
+        <h1>Total likes: {totalLikes}</h1>
+        <Quote
+          onLike={e => this.increaseFirstLikes()}
+          numLikes={this.state.firstQuoteLikes}
+          text="It is how it is"
+          author="Jerry"
+        />
+        <Quote
+          onLike={e => this.increaseSecondLikes()}
+          numLikes={this.state.secondQuoteLikes}
+          text="props are immutable"
+          author="Some React-Guy"
+        />
       </div>
     )
   }
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react'
 
 export default class Quote extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      numLikes: 0,
-    }
-  }
+  // wird ab jetzt außerhalb gemanaged:
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     numLikes: 0,
+  //   }
+  // }
 
-  increaseLikes() {
-    this.setState({ numLikes: this.state.numLikes + 1 })
-  }
+  // wird außerhalb gemacht:
+  // increaseLikes() {
+  //   this.setState({ numLikes: this.state.numLikes + 1 })
+  // }
 
   render() {
     return (
       <div>
         <q lang="de">{this.props.text}</q>
         <i>- {this.props.author}</i>
-        <button onClick={e => this.increaseLikes()}>
-          Like ({this.state.numLikes})
+        <button onClick={e => this.props.onLike()}>
+          Like ({this.props.numLikes})
         </button>
       </div>
     )
